7d5eba2cdbff58198141d02117af06245f37f809
Mohamed-550 <98844732+Mohamed-550@users.noreply.github.com>
2022-02-05 01:13:16 +0200

    Update program.ts

--- program.ts
+++ program.ts
@@ -4,19 +4,14 @@ import { Counter } from "./singelton/Counter";
 class Program {
     static PrintCounter() {
         const Counter1 = Counter.GetInstance();
-        const Counter2 = Counter.GetInstance();
+        Counter1.AddOne();
+        console.log("counter 1: " + Counter1.count);
 
-        Counter1.AddOne();
-        console.log("counter 1: " + Counter1.count);
-        Counter2.AddOne();
-        console.log("counter 1: " + Counter1.count);
-        Counter1.AddOne();
-        console.log("counter 1: " + Counter1.count);
+        const Counter2 = Counter.GetInstance();
         Counter2.AddOne();
         console.log("counter 2: " + Counter2.count);
     }
-
 }
 
-const RunProgram = new Program();
+
 Program.PrintCounter();
